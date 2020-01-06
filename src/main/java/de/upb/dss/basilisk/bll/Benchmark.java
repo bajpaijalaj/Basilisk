5b30dbb087ab46614de23732f29adf656721e00c
Ranjith K <ranjith.masthikatte@gmail.com>
2020-01-06 19:08:31 +0100

    Update Benchmark.java

--- src/main/java/de/upb/dss/basilisk/bll/Benchmark.java
+++ src/main/java/de/upb/dss/basilisk/bll/Benchmark.java
@@ -314,15 +314,16 @@ public class Benchmark {
 
         try
         {
+        	String path = Benchmark.class.getResource("/iguanaConfig.ftl").getPath().replace("iguanaConfig.ftl", "");
         	//Set up the free marker configuration, with template loading class and path.
-        	cfg.setClassForTemplateLoading(Benchmark.class, ".");
+        	cfg.setClassForTemplateLoading(Benchmark.class, path);
             cfg.setDefaultEncoding("UTF-8");
             
             //Get the Iguana configuration template.
             System.out.println(Benchmark.class.getResource("/iguanaConfig.ftl"));
             Scanner sc = new Scanner(System.in);
             sc.nextInt();
-        	Template template = cfg.getTemplate(Benchmark.class.getResource("/iguanaConfig.ftl").getFile().toString());
+        	Template template = cfg.getTemplate("iguanaConfig.ftl");
 
         	//Port number and query file to insert into benchmark template
             Map<String, Object> templateData = new HashMap<>();
